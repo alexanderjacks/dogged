c0819dfa92556d8b5f6a0cfd4f14528ede22b253
Alexander Jacks <alexanderthejacks@gmail.com>
2020-02-05 13:29:54 -0800

    :art: new header img for allCards page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,7 +10,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/moonlit.jpg')`,
+            backgroundImage: `url('/img/sshot-mine-as-archive.png')`,
           }}
         >
           <h1
