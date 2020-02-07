c2aabc23c5d40cd412f5816e06dbfa0c5ebae943
Alexander Jacks <alexanderthejacks@gmail.com>
2020-02-07 10:20:52 -0800

    🔬 md script almost complete

--- webscrapers/make_crops.py
+++ webscrapers/make_crops.py
@@ -8,15 +8,26 @@ import csv
 
 # opens file to work from (only read not write (rb not wb))
 with open('crops.csv', 'rb') as csvfile:
+    # var name b/c this is hecka raw data
     pulpy_data = csv.DictReader(csvfile, delimiter=',', quotechar='\"')
-    headers = ['templateKey', 'title', 'description', 'featuredpost', 'featuredimage', 'sellPrice', 'tags']
+    # want a way to populate this from the csv header! less errors
+    headers = ['templateKey', 'title', 'description', 'featuredpost', 'featuredimage', 'sellPrice', 'tags'] # manual entry for now
     fields = []
     for row in pulpy_data:
-        t3st_file_name = row['title']
-        fil3_nam3 = t3st_file_name.lower().replace(' ','-')
-        print(fil3_nam3) # gonna save the .md w this name
+        working_file_name = row['title'] # for db schema
+        file_name = working_file_name.lower().replace(' ','-') # b/c filesystem
+        print(file_name) # gonna save the .md w this name
+        #
+        # populates from the manual entry list
+        # there's gotta exist a smoother, more pythonic way...
         fields.extend((row[headers[0]],row[headers[1]],row[headers[2]],row[headers[3]],row[headers[4]],row[headers[5]],row[headers[6]]))
-        print("---")
+        # creates md
+        print("---") # for db schema
         for header, field in zip(headers, fields):
-            print("{} : {}.".format(header, field))
-        print("---")
+            # if field != "tags":
+            print("{}: {}.".format(header, field))
+            # else:
+            #     print("tags ought be: {}".format(field).split())
+        # need a 2nd loop to deal out tags field, by ','...
+        print("---") # for db schema
+        # need a method to write to .md file!
