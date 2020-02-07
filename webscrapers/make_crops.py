91f9a1e16a013af3dbd8fa3df4129bbd306cdbe3
Alexander Jacks <alexanderthejacks@gmail.com>
2020-02-07 11:27:28 -0800

    🔬 md script 95% complete

--- webscrapers/make_crops.py
+++ webscrapers/make_crops.py
@@ -11,23 +11,22 @@ with open('crops.csv', 'rb') as csvfile:
     # var name b/c this is hecka raw data
     pulpy_data = csv.DictReader(csvfile, delimiter=',', quotechar='\"')
     # want a way to populate this from the csv header! less errors
-    headers = ['templateKey', 'title', 'description', 'featuredpost', 'featuredimage', 'sellPrice', 'tags'] # manual entry for now
+    headers = ['templateKey', 'title', 'description', 'featuredpost', 'sellPrice', 'tags'] # manual entry for now
     fields = []
     for row in pulpy_data:
         working_file_name = row['title'] # for db schema
-        file_name = working_file_name.lower().replace(' ','-') # b/c filesystem
-        print(file_name) # gonna save the .md w this name
+        img_file_name = working_file_name.replace(' ','_') # b/c filesystem
+        md_file_name = working_file_name.lower().replace(' ','-') # b/c filesystem
         #
         # populates from the manual entry list
         # there's gotta exist a smoother, more pythonic way...
-        fields.extend((row[headers[0]],row[headers[1]],row[headers[2]],row[headers[3]],row[headers[4]],row[headers[5]],row[headers[6]]))
+        fields.extend((row[headers[0]],row[headers[1]],row[headers[2]],row[headers[3]],row[headers[4]],row[headers[5]]))
         # creates md
         print("---") # for db schema
+        print("featuredImage: /img/{}".format(img_file_name)) # for db schema
         for header, field in zip(headers, fields):
-            # if field != "tags":
-            print("{}: {}.".format(header, field))
-            # else:
-            #     print("tags ought be: {}".format(field).split())
+            print("{}: {}.".format(header, field).replace(',' , '\n  - '))
         # need a 2nd loop to deal out tags field, by ','...
         print("---") # for db schema
         # need a method to write to .md file!
+        print("Gonna save this one as {}.md\n".format(md_file_name)) # gonna save the .md w this name
