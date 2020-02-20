93481c4a96c23c6d9d459637cecb0bcf38e301d2
Alexander Jacks <alexanderthejacks@gmail.com>
2020-02-20 13:13:26 -0800

    🚀:bookmark: PWA configs set

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,18 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: { // PWA specs
+        name: `StardewDex`,
+        short_name: `StardewDex`,
+        start_url: `/`,
+        background_color: `#ffa31c`,
+        theme_color: `#0000ff`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`,  // PWA ingredient
+    'gatsby-plugin-netlify', // plz keep this last in the plugins array
   ],
 }
